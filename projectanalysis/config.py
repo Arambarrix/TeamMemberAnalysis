f68fe91a2aee6f8e43d9babb8a2518c458fd2a02
wtoure <wtoure@asi.fr>
2023-08-09 16:08:25 +0200

    feat(membre):Mise à jour de la configuration des membres

--- projectanalysis/config.py
+++ projectanalysis/config.py
@@ -2,6 +2,6 @@ import api_devs
 path = "http://localhost"
 port = 8080
 context = "api"
-membres_endpoints = api_devs.setPath(path,port,context,"membres")
+membres_endpoints = api_devs.setPath(path,port,context,"utilisateurs")
 equipes_endpoints =  api_devs.setPath(path,port,context,"equipes")
 projets_endpoints =  api_devs.setPath(path,port,context,"projets")
